1a6788c10096a0e6992e136c69342a2f4edd4e26
Roverp <yar.zubaha@proton.me>
2025-01-19 11:42:30 +0100

    For F.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -20,22 +20,18 @@ return {
     },
   },
 
-  -- {
-  --   "nvimtools/none-ls.nvim",
-  --   config = function()
-  --     local null_ls = require "null-ls"
-  --     null_ls.setup {
-  --       sources = {
-  --         null_ls.builtins.formatting.stylua,
-  --
-  --         null_ls.builtins.formatting.prettier,
-  --         -- null_ls.builtins.formatting.htmlbeautifier,
-  --         null_ls.builtins.diagnostics.eslint_d,
-  --
-  --         null_ls.builtins.formtting.black,
-  --         null_ls.builtins.diagnostics.ruff,
-  --       },
-  --     }
-  --   end,
-  -- },
+  {
+    "nvimtools/none-ls.nvim",
+    config = function()
+      local null_ls = require "null-ls"
+      null_ls.setup {
+        sources = {
+          null_ls.builtins.diagnostics.eslint_d.with {
+            diagnostics_format = "[eslint_d] #{m} (#{c})",
+          },
+        },
+        debug = true,
+      }
+    end,
+  },
 }
