33993e55e02a783f2f2ac690bb5929eb85534fa2
Roverp <yar.zubaha@proton.me>
2025-03-02 10:43:50 +0100

    Add render-markdown.nvim & change keymaps

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,18 +6,11 @@ local map = vim.keymap.set
 
 -- map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-map("n", "<leader>wt", "<CMD>Telescope keymaps<CR>", { desc = "Keymap cheatsheet in Telescope" })
 
 -- none-ls keybinds
 -- map("n", "<leader>gf", vim.lsp.buf.format, {})
 
-map("n", "<leader>gf", function()
-  require("conform").format { async = true }
-end, { desc = "Format code with Conform" })
-
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
-
-map("n", "<leader>sl", "<cmd>LiveServerToggle<cr>", { desc = "Toggle Live Server" })
 
 -- remaps for centring
 map("n", "<C-d>", "<C-d>zz", { desc = "Jump half a page down and center the screeen" })
@@ -36,4 +29,25 @@ map("n", "}", "}zz", { desc = "Jump to the end of the block and center the scree
 
 map("n", "x", '"_x', { desc = "Save deleted character to the underlying register" })
 
-map("n", "<leader>sa", "gg<S-v>G", { desc = "select all" })
+map("n", "<leader>sa", "gg<S-v>G", { desc = "󰒆 Select All" })
+
+-- ------------
+-- | PLUGINS  |
+-- ------------
+
+-- Live Server
+map("n", "<leader>tl", "<cmd>LiveServerToggle<cr>", { desc = " Toggle Live Server" })
+
+-- Minty color picker:
+map("n", "<leader>cc", "<cmd>lua require('minty.huefy').open()<cr>", { desc = " Open Minty Huefy color picker" })
+
+-- Render Markdown
+map("n", "<leader>tm", "<cmd>RenderMarkdown toggle<cr>", { desc = " Toggle Markdown Render" })
+
+-- Prettier (Conform)
+map("n", "<leader>gf", function()
+  require("conform").format { async = true }
+end, { desc = " Format code with Conform" })
+
+-- Telescope
+map("n", "<leader>wt", "<CMD>Telescope keymaps<CR>", { desc = "Keymap cheatsheet in Telescope" })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -24,24 +24,24 @@ return {
     },
   },
 
-  {
-    "nvimtools/none-ls.nvim",
-    config = function()
-      local null_ls = require "null-ls"
-      null_ls.setup {
-        sources = {
-          null_ls.builtins.diagnostics.eslint_d.with {
-            diagnostics_format = "[eslint_d] #{m} (#{c})",
-          },
-        },
-        debug = true,
-      }
-    end,
-  },
+  -- {
+  --   "nvimtools/none-ls.nvim",
+  --   config = function()
+  --     local null_ls = require "null-ls"
+  --     null_ls.setup {
+  --       sources = {
+  --         null_ls.builtins.diagnostics.eslint_d.with {
+  --           diagnostics_format = "[eslint_d] #{m} (#{c})",
+  --         },
+  --       },
+  --       debug = true,
+  --     }
+  --   end,
+  -- },
 
   {
     "windwp/nvim-ts-autotag",
-    lazy = false,
+    event = { "BufReadPre", "BufNewFile" },
     config = function()
       local autotag = require "nvim-ts-autotag"
 
@@ -81,94 +81,130 @@ return {
   },
 
   {
-    "3rd/image.nvim",
-    event = "VeryLazy",
-    -- dependencies = {
-    --   {
-    --     "nvim-treesitter/nvim-treesitter",
-    --     build = ":TSUpdate",
-    --     config = function()
-    --       require("nvim-treesitter.configs").setup {
-    --         ensure_installed = { "markdown" },
-    --         highlight = { enable = true },
-    --       }
-    --     end,
-    --   },
-    -- },
-    config = function()
-      require("image").setup {
-        backend = "kitty",
-        kitty_method = "normal",
-        integrations = {
-          -- Notice these are the settings for markdown files
-          markdown = {
-            enabled = true,
-            clear_in_insert_mode = false,
-            -- Set this to false if you don't want to render images coming from
-            -- a URL
-            download_remote_images = true,
-            -- Change this if you would only like to render the image where the
-            -- cursor is at
-            -- I set this to true, because if the file has way too many images
-            -- it will be laggy and will take time for the initial load
-            -- only_render_image_at_cursor = true,
-            only_render_image_at_cursor = vim.g.neovim_mode == "skitty" and false or true,
-            -- markdown extensions (ie. quarto) can go here
-            filetypes = { "markdown", "vimwiki", "html" },
-          },
-          neorg = {
-            enabled = true,
-            clear_in_insert_mode = false,
-            download_remote_images = true,
-            only_render_image_at_cursor = false,
-            filetypes = { "norg" },
-          },
-          -- This is disabled by default
-          -- Detect and render images referenced in HTML files
-          -- Make sure you have an html treesitter parser installed
-          -- ~/github/dotfiles-latest/neovim/neobean/lua/plugins/treesitter.lua
-          html = {
-            enabled = true,
-            only_render_image_at_cursor = true,
-            -- Enabling "markdown" below allows you to view html images in .md files
-            -- https://github.com/3rd/image.nvim/issues/234
-            -- filetypes = { "html", "xhtml", "htm", "markdown" },
-            filetypes = { "html", "xhtml", "htm" },
-          },
-          -- This is disabled by default
-          -- Detect and render images referenced in CSS files
-          -- Make sure you have a css treesitter parser installed
-          -- ~/github/dotfiles-latest/neovim/neobean/lua/plugins/treesitter.lua
-          css = {
-            enabled = true,
-          },
-        },
-        max_width = nil,
-        max_height = nil,
-        max_width_window_percentage = nil,
+    "echasnovski/mini.icons",
+    version = false,
+    -- dependencies = { "MeanderingProgrammer/render-markdown.nvim" },
+    opts = {
 
-        -- This is what I changed to make my images look smaller, like a
-        -- thumbnail, the default value is 50
-        -- max_height_window_percentage = 20,
-        -- max_height_window_percentage = 40,
-        -- 40 for my default nvim config and 30 for skitty
-        max_height_window_percentage = vim.g.neovim_mode == "skitty" and 30 or 40,
+      -- Icon style: 'glyph' or 'ascii'
+      style = "glyph",
 
-        -- toggles images when windows are overlapped
-        window_overlap_clear_enabled = false,
-        window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+      -- Customize per category. See `:h MiniIcons.config` for details.
+      default = {},
+      directory = {},
+      extension = {},
+      file = {},
+      filetype = {},
+      lsp = {},
+      os = {},
 
-        -- auto show/hide images when the editor gains/looses focus
-        -- editor_only_render_when_focused = true,
-        -- editor_only_render_when_focused = vim.g.neovim_mode == "skitty" and false or true,
-
-        -- auto show/hide images in the correct tmux window
-        -- In the tmux.conf add `set -g visual-activity off`
-        -- tmux_show_only_in_active_window = true,
-
-        -- render image files as images when opened
-        hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" },
-      }
-    end,
+      -- Control which extensions will be considered during "file" resolution
+      use_file_extension = function(ext, file)
+        return true
+      end,
+    },
   },
+
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you use standalone mini plugins
+    ft = { "markdown", "quarto" },
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
+      enabled = false,
+    },
+  },
+
+  -- {
+  --   "3rd/image.nvim",
+  --   event = "VeryLazy",
+  --   -- dependencies = {
+  --   --   {
+  --   --     "nvim-treesitter/nvim-treesitter",
+  --   --     build = ":TSUpdate",
+  --   --     config = function()
+  --   --       require("nvim-treesitter.configs").setup {
+  --   --         ensure_installed = { "markdown" },
+  --   --         highlight = { enable = true },
+  --   --       }
+  --   --     end,
+  --   --   },
+  --   -- },
+  --   config = function()
+  --     require("image").setup {
+  --       backend = "kitty",
+  --       kitty_method = "normal",
+  --       integrations = {
+  --         -- Notice these are the settings for markdown files
+  --         markdown = {
+  --           enabled = true,
+  --           clear_in_insert_mode = false,
+  --           -- Set this to false if you don't want to render images coming from
+  --           -- a URL
+  --           download_remote_images = true,
+  --           -- Change this if you would only like to render the image where the
+  --           -- cursor is at
+  --           -- I set this to true, because if the file has way too many images
+  --           -- it will be laggy and will take time for the initial load
+  --           -- only_render_image_at_cursor = true,
+  --           only_render_image_at_cursor = vim.g.neovim_mode == "skitty" and false or true,
+  --           -- markdown extensions (ie. quarto) can go here
+  --           filetypes = { "markdown", "vimwiki", "html" },
+  --         },
+  --         neorg = {
+  --           enabled = true,
+  --           clear_in_insert_mode = false,
+  --           download_remote_images = true,
+  --           only_render_image_at_cursor = false,
+  --           filetypes = { "norg" },
+  --         },
+  --         -- This is disabled by default
+  --         -- Detect and render images referenced in HTML files
+  --         -- Make sure you have an html treesitter parser installed
+  --         -- ~/github/dotfiles-latest/neovim/neobean/lua/plugins/treesitter.lua
+  --         html = {
+  --           enabled = true,
+  --           only_render_image_at_cursor = true,
+  --           -- Enabling "markdown" below allows you to view html images in .md files
+  --           -- https://github.com/3rd/image.nvim/issues/234
+  --           -- filetypes = { "html", "xhtml", "htm", "markdown" },
+  --           filetypes = { "html", "xhtml", "htm" },
+  --         },
+  --         -- This is disabled by default
+  --         -- Detect and render images referenced in CSS files
+  --         -- Make sure you have a css treesitter parser installed
+  --         -- ~/github/dotfiles-latest/neovim/neobean/lua/plugins/treesitter.lua
+  --         css = {
+  --           enabled = true,
+  --         },
+  --       },
+  --       max_width = nil,
+  --       max_height = nil,
+  --       max_width_window_percentage = nil,
+  --
+  --       -- This is what I changed to make my images look smaller, like a
+  --       -- thumbnail, the default value is 50
+  --       -- max_height_window_percentage = 20,
+  --       -- max_height_window_percentage = 40,
+  --       -- 40 for my default nvim config and 30 for skitty
+  --       max_height_window_percentage = vim.g.neovim_mode == "skitty" and 30 or 40,
+  --
+  --       -- toggles images when windows are overlapped
+  --       window_overlap_clear_enabled = false,
+  --       window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+  --
+  --       -- auto show/hide images when the editor gains/looses focus
+  --       -- editor_only_render_when_focused = true,
+  --       -- editor_only_render_when_focused = vim.g.neovim_mode == "skitty" and false or true,
+  --
+  --       -- auto show/hide images in the correct tmux window
+  --       -- In the tmux.conf add `set -g visual-activity off`
+  --       -- tmux_show_only_in_active_window = true,
+  --
+  --       -- render image files as images when opened
+  --       -- hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" },
+  --     }
+  --   end,
+  -- },
 }
