a1e9b1e23022da82e9223bc2cb59388809c44502
Roverp <yar.zubaha@proton.me>
2025-06-24 12:12:05 +0200

    configure prettier

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,13 +1,27 @@
+local util = require "conform.util"
+
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    css = { "prettier" },
-    html = { "prettier" },
-    javascriptreact = { "prettier" },
     python = { "black" },
-    markdown = { "prettier" },
-    -- c = { "clang-format" },
-    json = { "prettier" },
+    css = { "local_prettier", "prettierd", "prettier", stop_after_first = true },
+    html = { "local_prettier", "prettierd", "prettier", stop_after_first = true },
+    javascript = { "local_prettier", "prettierd", "prettier", stop_after_first = true },
+    typescript = { "local_prettier", "prettierd", "prettier", stop_after_first = true },
+    javascriptreact = { "local_prettier", "prettierd", "prettier", stop_after_first = true },
+    typescriptreact = { "local_prettier", "prettierd", "prettier", stop_after_first = true },
+    markdown = { "local_prettier", "prettierd", "prettier", stop_after_first = true },
+    json = { "local_prettier", "prettierd", "prettier", stop_after_first = true },
+  },
+
+  formatters = {
+    local_prettier = {
+      command = util.find_executable({
+        "./node_modules/.bin/prettier",
+      }, "prettier"),
+      args = { "--stdin-filepath", "$FILENAME" },
+      stdin = true,
+    },
   },
 
   notify_on_error = true,
