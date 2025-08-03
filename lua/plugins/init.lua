c8d94992db1eca56a9b62ee28e3c49101955c0ef
Roverp <yar.zubaha@proton.me>
2025-08-03 23:27:29 +0200

    feat(plugins): add claude-code.nvim and obsidian.nvim plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -140,97 +140,97 @@ return {
     end,
   },
 
-  {
-    "3rd/image.nvim",
-    event = "VeryLazy",
-    -- dependencies = {
-    --   {
-    --     "nvim-treesitter/nvim-treesitter",
-    --     build = ":TSUpdate",
-    --     config = function()
-    --       require("nvim-treesitter.configs").setup {
-    --         ensure_installed = { "markdown" },
-    --         highlight = { enable = true },
-    --       }
-    --     end,
-    --   },
-    -- },
-    config = function()
-      require("image").setup {
-        backend = "kitty",
-        kitty_method = "normal",
-        integrations = {
-          -- Notice these are the settings for markdown files
-          markdown = {
-            enabled = true,
-            clear_in_insert_mode = false,
-            -- Set this to false if you don't want to render images coming from
-            -- a URL
-            download_remote_images = true,
-            -- Change this if you would only like to render the image where the
-            -- cursor is at
-            -- I set this to true, because if the file has way too many images
-            -- it will be laggy and will take time for the initial load
-            -- only_render_image_at_cursor = true,
-            only_render_image_at_cursor = vim.g.neovim_mode == "skitty" and false or true,
-            -- markdown extensions (ie. quarto) can go here
-            filetypes = { "markdown", "vimwiki", "html" },
-          },
-          neorg = {
-            enabled = true,
-            clear_in_insert_mode = false,
-            download_remote_images = true,
-            only_render_image_at_cursor = false,
-            filetypes = { "norg" },
-          },
-          -- This is disabled by default
-          -- Detect and render images referenced in HTML files
-          -- Make sure you have an html treesitter parser installed
-          -- ~/github/dotfiles-latest/neovim/neobean/lua/plugins/treesitter.lua
-          html = {
-            enabled = true,
-            only_render_image_at_cursor = true,
-            -- Enabling "markdown" below allows you to view html images in .md files
-            -- https://github.com/3rd/image.nvim/issues/234
-            -- filetypes = { "html", "xhtml", "htm", "markdown" },
-            filetypes = { "html", "xhtml", "htm" },
-          },
-          -- This is disabled by default
-          -- Detect and render images referenced in CSS files
-          -- Make sure you have a css treesitter parser installed
-          -- ~/github/dotfiles-latest/neovim/neobean/lua/plugins/treesitter.lua
-          css = {
-            enabled = true,
-          },
-        },
-        max_width = nil,
-        max_height = nil,
-        max_width_window_percentage = nil,
-
-        -- This is what I changed to make my images look smaller, like a
-        -- thumbnail, the default value is 50
-        -- max_height_window_percentage = 20,
-        -- max_height_window_percentage = 40,
-        -- 40 for my default nvim config and 30 for skitty
-        max_height_window_percentage = vim.g.neovim_mode == "skitty" and 30 or 40,
-
-        -- toggles images when windows are overlapped
-        window_overlap_clear_enabled = false,
-        window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
-
-        -- auto show/hide images when the editor gains/looses focus
-        -- editor_only_render_when_focused = true,
-        -- editor_only_render_when_focused = vim.g.neovim_mode == "skitty" and false or true,
-
-        -- auto show/hide images in the correct tmux window
-        -- In the tmux.conf add `set -g visual-activity off`
-        -- tmux_show_only_in_active_window = true,
-
-        -- render image files as images when opened
-        -- hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" },
-      }
-    end,
-  },
+  -- {
+  --   "3rd/image.nvim",
+  --   event = "VeryLazy",
+  --   -- dependencies = {
+  --   --   {
+  --   --     "nvim-treesitter/nvim-treesitter",
+  --   --     build = ":TSUpdate",
+  --   --     config = function()
+  --   --       require("nvim-treesitter.configs").setup {
+  --   --         ensure_installed = { "markdown" },
+  --   --         highlight = { enable = true },
+  --   --       }
+  --   --     end,
+  --   --   },
+  --   -- },
+  --   config = function()
+  --     require("image").setup {
+  --       backend = "kitty",
+  --       kitty_method = "normal",
+  --       integrations = {
+  --         -- Notice these are the settings for markdown files
+  --         markdown = {
+  --           enabled = true,
+  --           clear_in_insert_mode = false,
+  --           -- Set this to false if you don't want to render images coming from
+  --           -- a URL
+  --           download_remote_images = true,
+  --           -- Change this if you would only like to render the image where the
+  --           -- cursor is at
+  --           -- I set this to true, because if the file has way too many images
+  --           -- it will be laggy and will take time for the initial load
+  --           -- only_render_image_at_cursor = true,
+  --           only_render_image_at_cursor = vim.g.neovim_mode == "skitty" and false or true,
+  --           -- markdown extensions (ie. quarto) can go here
+  --           filetypes = { "markdown", "vimwiki", "html" },
+  --         },
+  --         neorg = {
+  --           enabled = true,
+  --           clear_in_insert_mode = false,
+  --           download_remote_images = true,
+  --           only_render_image_at_cursor = false,
+  --           filetypes = { "norg" },
+  --         },
+  --         -- This is disabled by default
+  --         -- Detect and render images referenced in HTML files
+  --         -- Make sure you have an html treesitter parser installed
+  --         -- ~/github/dotfiles-latest/neovim/neobean/lua/plugins/treesitter.lua
+  --         html = {
+  --           enabled = true,
+  --           only_render_image_at_cursor = true,
+  --           -- Enabling "markdown" below allows you to view html images in .md files
+  --           -- https://github.com/3rd/image.nvim/issues/234
+  --           -- filetypes = { "html", "xhtml", "htm", "markdown" },
+  --           filetypes = { "html", "xhtml", "htm" },
+  --         },
+  --         -- This is disabled by default
+  --         -- Detect and render images referenced in CSS files
+  --         -- Make sure you have a css treesitter parser installed
+  --         -- ~/github/dotfiles-latest/neovim/neobean/lua/plugins/treesitter.lua
+  --         css = {
+  --           enabled = true,
+  --         },
+  --       },
+  --       max_width = nil,
+  --       max_height = nil,
+  --       max_width_window_percentage = nil,
+  --
+  --       -- This is what I changed to make my images look smaller, like a
+  --       -- thumbnail, the default value is 50
+  --       -- max_height_window_percentage = 20,
+  --       -- max_height_window_percentage = 40,
+  --       -- 40 for my default nvim config and 30 for skitty
+  --       max_height_window_percentage = vim.g.neovim_mode == "skitty" and 30 or 40,
+  --
+  --       -- toggles images when windows are overlapped
+  --       window_overlap_clear_enabled = false,
+  --       window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+  --
+  --       -- auto show/hide images when the editor gains/looses focus
+  --       -- editor_only_render_when_focused = true,
+  --       -- editor_only_render_when_focused = vim.g.neovim_mode == "skitty" and false or true,
+  --
+  --       -- auto show/hide images in the correct tmux window
+  --       -- In the tmux.conf add `set -g visual-activity off`
+  --       -- tmux_show_only_in_active_window = true,
+  --
+  --       -- render image files as images when opened
+  --       -- hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" },
+  --     }
+  --   end,
+  -- },
 
   -- {
   --   "HiPhish/rainbow-delimiters.nvim",
@@ -411,7 +411,7 @@ return {
       { "<leader>z", "<cmd>SimpleZoomToggle<CR>", desc = "Zoom toggle" },
     },
     opts = {
-      hide_tabline = true,
+      hide_tabline = false,
     },
   },
 
@@ -437,5 +437,109 @@ return {
       }
       require("qmk").setup(conf)
     end,
+  },
+
+  {
+    "greggh/claude-code.nvim",
+    lazy = "VeryLazy",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- Required for git operations
+    },
+    config = function()
+      require("claude-code").setup {
+        window = {
+          position = "float",
+
+          float = {
+            width = "85%",
+            height = "85%",
+          },
+        },
+        keymaps = {
+          toggle = {
+            normal = "<M-c>",
+            terminal = "<M-c>",
+          },
+        },
+      }
+    end,
+  },
+
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*",
+    lazy = true,
+    ft = "markdown",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      -- Dynamic workspace: automatically detects vault from current file
+      workspaces = {
+        {
+          name = "auto",
+          path = function()
+            -- This function finds the nearest .obsidian directory upward from current file
+            local current_file = vim.api.nvim_buf_get_name(0)
+            if current_file == "" then
+              return vim.fn.getcwd() -- fallback to current directory
+            end
+
+            local dir = vim.fn.fnamemodify(current_file, ":h")
+
+            -- Search upward for .obsidian directory (indicates vault root)
+            while dir ~= "/" and dir ~= "" do
+              if vim.fn.isdirectory(dir .. "/.obsidian") == 1 then
+                return dir
+              end
+              dir = vim.fn.fnamemodify(dir, ":h")
+            end
+
+            -- If no .obsidian found, use the directory of the current file
+            return vim.fn.fnamemodify(current_file, ":h")
+          end,
+        },
+      },
+
+      -- Basic settings that work without strict workspace requirements
+      new_notes_location = "current_dir", -- Create notes in current directory
+      notes_subdir = vim.NIL, -- Don't use a subdirectory
+
+      completion = {
+        nvim_cmp = true,
+        min_chars = 2,
+      },
+
+      mappings = {
+        ["gf"] = {
+          action = function()
+            return require("obsidian").util.gf_passthrough()
+          end,
+          opts = { noremap = false, expr = true, buffer = true },
+        },
+        ["<leader>ch"] = {
+          action = function()
+            return require("obsidian").util.toggle_checkbox()
+          end,
+          opts = { buffer = true },
+        },
+      },
+
+      -- Disable features that require specific directory structures
+      daily_notes = {
+        folder = vim.NIL,
+      },
+      templates = {
+        folder = vim.NIL,
+      },
+
+      ui = {
+        enable = false,
+        -- checkboxes = {
+        --   [" "] = { char = "☐", hl_group = "ObsidianTodo" },
+        --   ["x"] = { char = "✔", hl_group = "ObsidianDone" },
+        -- },
+      },
+    },
   },
 }
