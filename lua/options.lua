cc35be01c31de14e35efc1ae7dc2aeccf670b9e0
Roverp <yar.zubaha@proton.me>
2025-08-17 23:27:36 +0200

    feat(options): set tabwidth to 4

--- lua/options.lua
+++ lua/options.lua
@@ -9,5 +9,10 @@ o.relativenumber = true
 -- o.cmdheight = 0
 o.scrolloff = 5
 
+-- Default indentation (4 spaces)
+o.tabstop = 4
+o.shiftwidth = 4
+o.expandtab = true
+
 -- vim.opt.foldmethod = "expr"
 -- vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
